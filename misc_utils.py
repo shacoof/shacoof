ab1ffef719fc8536cf2f2117420327489bc1359a
SharonCohenOfirTrilogy <Sharon.Cohenofir@trilogy.com>
2020-12-31 14:54:25 +0200

    Adding daily backlog functionality

--- misc_utils.py
+++ misc_utils.py
@@ -1,7 +1,19 @@
 
 from datetime import datetime
+import csv
+
+
+def createCSV(filename,list):   
+    with open(filename, 'w', newline='') as file:
+        writer = csv.writer(file)
+        for r in list:
+            print(r)
+            writer.writerow(r)
+
+
 def debug_print(app_logger, str):
     app_logger.debug(datetime.now().ctime() + " :: " + str) 
+    print(str)
     return str+'<br>'
 
 def to_object(item):
